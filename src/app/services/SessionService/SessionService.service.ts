d1f4fe572bea84fe92fb41e3d64de96b71622d02
BeratKARATAS53 <b21527142@cs.hacettepe.edu.tr>
2020-06-21 17:03:16 +0300

    Logout Function Added

--- src/app/services/SessionService/SessionService.service.ts
+++ src/app/services/SessionService/SessionService.service.ts
@@ -10,7 +10,7 @@ export const TOKEN_NAME: string = 'token';
 export class SessionService {
     private headers = new Headers({ 'Content-Type': 'application/json' });
 
-    constructor(private router: Router, private registerComp: RegisterPageComponent) {}
+    constructor(private router: Router) {}
     ngOnInit() {}
 
     getToken(): string {
@@ -25,12 +25,12 @@ export class SessionService {
             .put({ username: username, password: password })
             .then(() => {
                 alert('Kayıt Başarılı');
-                this.registerComp.submitted = true
+                // this.registerComp.submitted = true
                 // Eğer veritabanında böyle bir kullanıcı yoksa yeni kullanıcıyı ekliyorum ve login sayfasına yönlendiriyorum.
                 this.router.navigateByUrl('/login');
             })
             .catch((error) => {
-                this.registerComp.submitted = false
+                // this.registerComp.submitted = false
                 alert(
                     'Bu İsim Kullanılmakta! Başka Bir Kullanıcı Adı Deneyiniz!' +
                         error
@@ -46,15 +46,20 @@ export class SessionService {
                 list.forEach((item) => {
                     if (item) {
                         alert('Giriş Başarılı');
-                        this.registerComp.loggedIn = true
+                        // this.registerComp.loggedIn = true
                         this.setToken(item.username); // Eğer giriş başarılı ise token'ı local storage'a kaydediyorum.
                         this.router.navigateByUrl('/'); // Ardından Ana Sayfaya yönlendiriyorum.
                     }
                 });
             })
             .catch((error) => {
-                this.registerComp.loggedIn = true
+                // this.registerComp.loggedIn = true
                 alert('Hatalı Ya Da Eksik Bilgi Girdiniz!');
             });
+    }
+
+    logOut(){
+        localStorage.clear();
+        window.location.reload();
     }
 }
