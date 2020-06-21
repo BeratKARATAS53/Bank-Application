ff7192700e10aad1e78ba048523126f48f5b33a9
BeratKARATAS53 <b21527142@cs.hacettepe.edu.tr>
2020-06-21 15:30:30 +0300

    Transfers Component Name Changed To TransferPage

---
+++ src/app/pages/TransfersPage/TransfersPage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-TransfersPage',
+  templateUrl: './TransfersPage.component.html',
+  styleUrls: ['./TransfersPage.component.css']
+})
+export class TransfersPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
