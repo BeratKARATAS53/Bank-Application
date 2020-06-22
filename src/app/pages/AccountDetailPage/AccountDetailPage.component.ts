16fa06e302cef2bfff2942f02616658062664a52
BeratKARATAS53 <b21527142@cs.hacettepe.edu.tr>
2020-06-22 17:26:39 +0300

    Account Detail Page Updated

--- src/app/pages/AccountDetailPage/AccountDetailPage.component.ts
+++ src/app/pages/AccountDetailPage/AccountDetailPage.component.ts
@@ -1,3 +1,11 @@
+import { SessionService } from 'src/app/services/SessionService/SessionService.service';
+import {
+    AccountService,
+    numberOfAccounts,
+    getAccount,
+} from 'src/app/services/AccountService/AccountService.service';
+import { Account } from './../../models/Account';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +14,42 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./AccountDetailPage.component.css'],
 })
 export class AccountDetailPageComponent implements OnInit {
-    constructor() {}
+    account: Account;
+    accountNumber: number;
+    username: string;
+    numberOfAccounts: number;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private session: SessionService,
+        private accountService: AccountService
+    ) {
+        if (!session.getToken()) {
+            // Eğer giriş yapan kullanıcı yoksa Login sayfasına yönlendirilir.
+            this.router.navigateByUrl('/login');
+        } else {
+            // Eğer giriş yapan kullanıcı varsa token'dan kullanıcı adı bilgisi alınır.
+            this.getFirst(session.getToken());
+        }
+    }
+
+    async getFirst(username: string) {
+        this.username = this.session.getToken();
+        await numberOfAccounts(username).then(
+            (resolve) => (this.numberOfAccounts = resolve)
+        );
+        await getAccount(
+            this.username,
+            this.route.snapshot.params.accountNumber
+        ).then((resolve) => {
+            this.account = resolve[0];
+        });
+    }
 
     ngOnInit() {}
+
+    logOut() {
+        this.session.logOut();
+    }
 }
