4e9f127897f94f0017b5a7b41a41a82f6d521e03
BeratKARATAS53 <b21527142@cs.hacettepe.edu.tr>
2020-06-27 14:28:58 +0300

    NgBootstrap Module & Sidebar Component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
+import { SidebarComponent } from './components/Sidebar/Sidebar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
-    declarations: [AppComponent, routingComponent],
+    declarations: [AppComponent, routingComponent, SidebarComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
         FormsModule,
         ReactiveFormsModule,
+        CommonModule,
+        NgbModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
