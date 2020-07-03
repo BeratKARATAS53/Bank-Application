b737bb8bbb3d8f4df1fed4f35df6062f5bbf98fa
BeratKARATAS53 <b21527142@cs.hacettepe.edu.tr>
2020-07-03 21:53:01 +0300

    MyCurrency And AbsoluteValue Pipes Added

---
+++ src/app/pipes/absoluteValue/absoluteValue.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AbsoluteValuePipe } from './absoluteValue.pipe';
+
+describe('Pipe: AbsoluteValuee', () => {
+  it('create an instance', () => {
+    let pipe = new AbsoluteValuePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/myCurrency/myCurrency.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'myCurrency',
+})
+export class MyCurrencyPipe implements PipeTransform {
+    transform(value: any): any {
+        let result: number = Math.round(value * 100) / 100;
+        return result;
+    }
+}
